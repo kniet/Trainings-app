014c5bd4e97f709246d06e462ce73764af87997c
Jakub <k.nietupski99@gmail.com>
2023-05-13 14:35:48 +0200

    Add data to database operation

--- trainings-api/index.js
+++ trainings-api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mysql from "mysql";
+import multer from "multer";
+import path from "path";
 
 const app = express();
 app.use(cors());
@@ -11,6 +13,22 @@ const db = mysql.createConnection({
   user: "root",
   password: "root",
   database: "trainingsapp",
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads"); // './public/images/' directory name where save the file
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
 });
 
 app.post("/login", (req, res) => {
@@ -34,12 +52,35 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/trainings", (req, res) => {
-    const sql =
-      "SELECT id, userimg, title, description, lessons, hours FROM training";
-    db.query(sql, (err, result) => {
-      if (err) return res.json("Error");
-        return res.json(result);
-    });
+  const sql =
+    "SELECT id, userimg, title, description, lessons, hours FROM training";
+  db.query(sql, (err, result) => {
+    if (err) return res.json("Error");
+    return res.json(result);
+  });
+});
+
+app.post("/addData", upload.single("image"), (req, res) => {
+    if (!req.file) {
+      console.log("No file upload");
+    } else {
+        const imgsrc = req.file.filename;
+        console.log(req.file);
+        db.query(
+          "INSERT INTO training SET ?",
+          {
+            title: req.body.title,
+            lessons: req.body.lessons,
+            description: req.body.description,
+            hours: req.body.hours,
+            userimg: imgsrc,
+          },
+          (err, result) => {
+            if (err) return res.json({ Message: "Error" });
+              return res.json({ Status: "Success" });
+          }
+        );
+      }
   });
 
 app.listen(8080, () => {
