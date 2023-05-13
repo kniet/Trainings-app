38012789949ba14fd7ba23d3543f752144e7a113
Jakub <k.nietupski99@gmail.com>
2023-05-13 14:30:15 +0200

    Login operation

--- trainings-api/index.js
+++ trainings-api/index.js
@@ -7,13 +7,32 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "trainingsapp",
-  });
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "trainingsapp",
+});
 
-  app.listen(8080, () => {
-    console.log("Connected to backed!");
-  });
-  
+app.post("/login", (req, res) => {
+  const login = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT * FROM user WHERE login = ? AND password = ?",
+    [login, password],
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send({ message: "Wrong username/password!" });
+      }
+    }
+  );
+});
+
+app.listen(8080, () => {
+  console.log("Connected to backed!");
+});
