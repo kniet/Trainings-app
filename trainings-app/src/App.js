4432739a5d805596e013a6b844b404d42ee54cac
Jakub <k.nietupski99@gmail.com>
2023-05-16 12:42:53 +0200

    Card component configured

--- trainings-app/src/App.js
+++ trainings-app/src/App.js
@@ -2,18 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import { useState } from "react";
-import { UserContext } from "./context/Context";
+import { IdContext, UserContext } from "./context/Context";
 
 const App = () => {
   const [isAdmin, setIsAdmin] = useState(0);
+  const [id, setId] = useState(0);
 
   return (
     <UserContext.Provider value={[isAdmin, setIsAdmin]}>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/homePage" element={<HomePage />} />
-      </Routes>
-      </UserContext.Provider>
+      <IdContext.Provider value={[id, setId]}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/homePage" element={<HomePage />} />
+        </Routes>
+      </IdContext.Provider>
+    </UserContext.Provider>
   );
 };
 
--- trainings-app/src/components/Card.js
+++ trainings-app/src/components/Card.js
@@ -1,43 +1,107 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import TextLabel from "./TextLabel";
 import Button from "./Button";
 import AdminButton from "./AdminButton";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { IdContext } from "../context/Context";
 
-function Card() {
-  return (
-    <div className="card">
-      <div className="trainingLogoDiv">
-        <img
-          class="trainingLogo"
-          src={require("../images/logo.png")}
-          alt="there should be a logo"
-        ></img>
-      </div>
-      <div className="title">
-        <TextLabel className="title">TEST123</TextLabel>
-      </div>
+function Card({ training }) {
+  const navigate = useNavigate();
+  const [id, setId] = useContext(IdContext);
+  const cardId = training.id;
+  const [isAdmin, setIsAdmin] = useState(Cookies.get("isAdmin") === "true");
+  const [registered, setRegistered] = useState(
+    Cookies.get(`isRegistered${cardId}`) === "true"
+  );
+  const [label, setLabel] = useState("Register");
 
-      <div className="description">
-        <TextLabel className="description">TEST123</TextLabel>
-      </div>
-      <div className="aboutCourse">
-        <div class="information">
+  useEffect(() => {
+    if (Cookies.get(`isRegistered${cardId}`) === "true") {
+      setLabel("Unregister");
+    }
+  });
+
+  const handleRegistered = () => {
+    const cardId = training.id;
+    const isRegistered = Cookies.get(`isRegistered${cardId}`) === "true";
+
+    if (!isRegistered) {
+      setRegistered(true);
+      setLabel("Unregister");
+      Cookies.set(`isRegistered${cardId}`, true);
+    } else {
+      setRegistered(false);
+      setLabel("Register");
+      Cookies.set(`isRegistered${cardId}`, false);
+    }
+  };
+
+  const handleUpdate = () => {
+    setId(cardId);
+    navigate("/updateTraining");
+  };
+
+  const handleDelete = () => {
+    console.log("test");
+  };
+
+  const mappedTraining = Object.keys(training).map((el, idx) => {
+    if (el === "lessons" || el === "hours" || el === "id") {
+      return null; // Return null for these keys since we're rendering them separately
+    }
+    if (el === "userimg") {
+      return (
+        <div className="trainingLogoDiv">
           <img
-            class="icon"
-            src={require("../images/lessons.png")}
-            alt="Lessons"
-          />
-          <span className="spanUser">10</span>
+            class="trainingLogo"
+            src={`http://localhost:8080/uploads/` + training[el]}
+            alt="there should be a logo"
+          ></img>
         </div>
-        <div class="information">
-          <img class="icon" src={require("../images/time.png")} alt="Time" />
-          <span className="spanUser">10</span>
-        </div>
+      );
+    }
+    return (
+      <div key={idx} className={el}>
+        <TextLabel className={el}>{training[el]}</TextLabel>
       </div>
-      <Button label={"Button"}></Button>
-      <AdminButton></AdminButton>
-      <TextLabel children="Registered!" className="registerLabel"></TextLabel>
+    );
+  });
+
+  const lessonsAndHours = (
+    <div className="aboutCourse">
+      <div class="information">
+        <img
+          class="icon"
+          src={require("../images/lessons.png")}
+          alt="Lessons"
+        />
+        <span className="spanUser">{training.lessons}</span>
+      </div>
+      <div class="information">
+        <img class="icon" src={require("../images/time.png")} alt="Time" />
+        <span className="spanUser">{training.hours}</span>
+      </div>
     </div>
+  );
+
+  return (
+    <>
+      {mappedTraining}
+      {lessonsAndHours}
+      {isAdmin ? (
+        <AdminButton
+          onUpdate={handleUpdate}
+          onDelete={handleDelete}
+        ></AdminButton>
+      ) : (
+        <Button label={label} onClick={handleRegistered}></Button>
+      )}
+      {registered && !isAdmin && (
+        <TextLabel children="Registered!" className="registerLabel"></TextLabel>
+      )}
+    </>
   );
 }
 
