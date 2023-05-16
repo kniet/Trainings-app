9960745cda3b76f5f01f7ac7fdb9aa740f987768
Jakub <k.nietupski99@gmail.com>
2023-05-16 12:37:55 +0200

    HomePage backend connection

--- trainings-app/src/pages/HomePage.js
+++ trainings-app/src/pages/HomePage.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import "../styles/mainPage.css";
 import Card from "../components/Card";
+import Cookies from "js-cookie";
+import axios from "axios";
+import TextLabel from "../components/TextLabel";
 
 function HomePage() {
+  const [trainings, setTrainings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [isAdmin, setIsAdmin] = useState(Cookies.get("isAdmin") === "true");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/trainings")
+      .then((res) => {
+        setTrainings(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err.message);
+      });
+  }, []);
+
+  const mappedTrainings = trainings.map((el, idx) => (
+    <div key={idx} className="card">
+      <Card training={el} />
+    </div>
+  ));
+
   return (
     <>
       <Header
         value="Create training"
         isCreateTrainingPage={false}
         action={"/createTraining"}
-        isAdmin={true}
+        isAdmin={isAdmin}
       />
       <div class="mainUser">
-        <Card />
+        {isLoading ? (
+          <TextLabel>Loading...</TextLabel>
+        ) : error ? (
+          <TextLabel>{error}</TextLabel>
+        ) : (
+          mappedTrainings
+        )}
       </div>
     </>
   );
